bfd636b78066e0743c50f208ad72698ab24de9fb
Dmitri <vdv150387@yandex.ru>
2022-02-06 15:36:04 +0500

    Initial commit

---
+++ app/src/main/java/com/demo/colordescriptor/MainActivity.java
@@ -0,0 +1,32 @@
+package com.demo.colordescriptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinnerColors;
+    private TextView textViewDescriptionTemp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinnerColors=findViewById(R.id.spinner3);
+        textViewDescriptionTemp=findViewById(R.id.textViewDescriptionTemp);
+
+    }
+
+    public void showDiscription(View view) {
+        int position = spinnerColors.getSelectedItemPosition();
+        String description= getDescriptionByPosition(position);
+        textViewDescriptionTemp.setText(description);
+    }
+    private String getDescriptionByPosition(int position){
+        String[] descriptions= getResources().getStringArray(R.array.description_of_temp);
+        return descriptions[position];
+    }
+}
